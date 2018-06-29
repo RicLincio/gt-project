5bd5ce08ae8bae0427e16600f741418875867cf1
Guglielmo <guglielmocamporese@gmail.com>
2018-06-29 21:55:39 +0200

    update

--- FFGAN_MNIST/main.py
+++ FFGAN_MNIST/main.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot as plt
 from FFGAN import GAN
 
 # Load MNIST data
-(x_tr, _), (x_te, _) = mnist.load_data()
-x = np.concatenate([x_tr, x_te])
-x = (x_tr.astype(np.float32) - 127.5)/127.5
-x = x_tr.reshape(-1, 784)
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = (X_train.astype(np.float32) - 127.5)/127.5
+X_train = X_train.reshape(60000, 784)
 
 # Model
 alpha = [0.1, 0.3, 0.5, 0.7, 0.9]
 for a in alpha:
 	model = GAN(alpha=a, lr=1e-4)
-	model.train(x, epochs=100, batch_size=256, generate_every=5)
+	model.train(X_train, epochs=100, batch_size=256, generate_every=5)
 
 

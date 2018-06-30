be54f1e5b9aa548c9b616f0b16e440815eeec9f8
RicLincio <37301469+RicLincio@users.noreply.github.com>
2018-06-30 21:39:50 +0200

    Results with _100_NS_big

--- FFGAN_MNIST/main.py
+++ FFGAN_MNIST/main.py
@@ -10,9 +10,9 @@ X_train = (X_train.astype(np.float32) - 127.5)/127.5
 X_train = X_train.reshape(60000, 784)
 
 # Model
-# alpha = [0.1, 0.3, 0.5, 0.7, 0.9]
-#for a in alpha:
-model = GAN(alpha=0.3, lr=1e-4)
-model.train(X_train, epochs=100, batch_size=256, generate_every=5)
+alpha = [0.5, 0.7, 0.9]
+for a in alpha:
+    model = GAN(alpha=a, lr=1e-4)
+    model.train(X_train, epochs=100, batch_size=256, generate_every=5)
 
 
